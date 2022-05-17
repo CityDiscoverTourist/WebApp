b38ad2c096570a43629c483d3b54f29fc6f50891
ssangnd <sangndxyz@gmail.com>
2022-05-17 23:12:41 +0700

    add ng select

--- src/app/pages/location/location-list/location-list.module.ts
+++ src/app/pages/location/location-list/location-list.module.ts
@@ -4,8 +4,9 @@ import { CommonModule } from '@angular/common';
 import { LocationListRoutingModule } from './location-list-routing.module';
 import { LocationListComponent } from './location-list.component';
 import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { NgSelectModule } from '@ng-select/ng-select';
 
-const lib = [NgxDatatableModule];
+const lib = [NgSelectModule, NgxDatatableModule];
 
 @NgModule({
   declarations: [
