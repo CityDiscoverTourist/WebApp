f85beec9dd9ef5420903a839deb2b2e7d489d87d
ssangnd <sangndxyz@gmail.com>
2022-05-23 21:57:49 +0700

    add toggle for quest description

--- src/app/pages/quest/quest-create/quest-create.component.ts
+++ src/app/pages/quest/quest-create/quest-create.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { RxState } from '@rx-angular/state';
+
+interface QuestEditState {
+  showQuestDescription: boolean;
+}
 
 @Component({
   selector: 'app-quest-create',
   templateUrl: './quest-create.component.html',
-  styleUrls: ['./quest-create.component.scss']
+  styleUrls: ['./quest-create.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [RxState],
 })
 export class QuestCreateComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private state: RxState<QuestEditState>,
+  ) { 
+    this.state.set({
+      showQuestDescription: false,
+    });
+  }
 
   ngOnInit(): void {
+    this.state.connect(this.toggleDescription$, (prev) => ({
+      showQuestDescription: !prev.showQuestDescription,
+    }));
   }
+
+  toggleDescription$ = new Subject<void>();
 
 }
