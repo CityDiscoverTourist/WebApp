9154a2051fa62c6e6199f0f5371fab061b89ab93
ssangnd <sangndxyz@gmail.com>
2022-05-23 23:43:08 +0700

    connect service with state

--- src/app/pages/quest/quest.component.ts
+++ src/app/pages/quest/quest.component.ts
@@ -1,7 +1,8 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { RxState } from '@rx-angular/state';
-import { QuestService } from 'src/app/services';
+import { AreaService, CityService, QuestService, QuestTypeService } from 'src/app/services';
 import { QuestListPageState, QUEST_PAGE_STATE } from './quest-list/states';
+import { QuestState, QUEST_STATE } from './states/quest.state';
 
 @Component({
   selector: 'app-quest',
@@ -12,16 +13,17 @@ import { QuestListPageState, QUEST_PAGE_STATE } from './quest-list/states';
 export class QuestComponent implements OnInit {
 
   constructor(
-    // private readonly categoryServices: CategoryService,
-   private questService: QuestService,
-    @Inject(QUEST_PAGE_STATE) private productPageState: RxState<QuestListPageState>
+    private readonly questTypeService:QuestTypeService,
+    @Inject(QUEST_STATE) private questState: RxState<QuestState>,
+    private readonly areaService:AreaService
   ) {
-   
+    questState.connect(questTypeService.getAreaType(),(_,curr)=>({
+      questTypeIds:curr
+    }));
+    questState.connect(areaService.getAreaType(),(_,curr)=>({
+      areaIds:curr
+    }));
   }
-
   ngOnInit(): void {
-    // this.productPageState.connect(this.questService.getCategories(), (_, curr) => ({
-    //   categories: curr,
-    // }));
   }
 }
