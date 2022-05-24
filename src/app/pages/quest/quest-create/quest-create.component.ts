c79ec3c39a4e475c49e702972ef9b25ffecb5a6c
ssangnd <sangndxyz@gmail.com>
2022-05-24 23:06:28 +0700

    add form and control for quest create

--- src/app/pages/quest/quest-create/quest-create.component.ts
+++ src/app/pages/quest/quest-create/quest-create.component.ts
@@ -1,8 +1,14 @@
-import { ChangeDetectionStrategy, Component, Inject, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Inject,
+  OnInit,
+} from '@angular/core';
 import { Observable, Subject, tap } from 'rxjs';
 import { RxState } from '@rx-angular/state';
 import { QuestState, QUEST_STATE } from '../states/quest.state';
 import { IdValue } from 'src/app/models';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 interface QuestEditState {
   showQuestDescription: boolean;
@@ -16,30 +22,55 @@ interface QuestEditState {
   providers: [RxState],
 })
 export class QuestCreateComponent implements OnInit {
-
   constructor(
     @Inject(QUEST_STATE) private questState: RxState<QuestState>,
     private state: RxState<QuestEditState>,
-  ) { 
+    private fb: FormBuilder
+  ) {
     this.state.set({
       showQuestDescription: false,
     });
   }
 
   ngOnInit(): void {
-    this.state.connect(this.toggleDescription$, (prev) => ({
+    this.state.connect(this.toggleDescription$, (prev,_) => ({
       showQuestDescription: !prev.showQuestDescription,
     }));
+
+    this.initForm();
   }
 
   toggleDescription$ = new Subject<void>();
-
-  get questTypeIds():Observable<IdValue[]>{
-    return this.questState.select('questTypeIds').pipe(tap((m) => console.log(m)));
+  get vm$():Observable<QuestEditState>{
+    return this.state.select();
   }
 
-  get areaIds():Observable<IdValue[]>{
-    return this.questState.select('areaIds').pipe(tap((m)=>console.log(m)));
+  get questTypeIds(): Observable<IdValue[]> {
+    return this.questState
+      .select('questTypeIds')
+      .pipe(tap((m) => console.log(m)));
   }
 
+  get areaIds(): Observable<IdValue[]> {
+    return this.questState.select('areaIds').pipe(tap((m) => console.log(m)));
+  }
+
+  form!: FormGroup;
+
+  initForm() {
+    this.form = this.fb.group({
+      title: ['', [Validators.required, Validators.minLength(5)]],
+      // sku: [null, [customProductSkuValidator()]],
+      description: [],
+      price: [],
+      estimatedTime: [],
+      image:[],
+      availableTime: [],
+      questTypeId: [],
+      // salePrice: [0, [Validators.required, Validators.min(10)]],
+      questOwnerId: [],
+      areaId: [],
+      status: [],
+    });
+  }
 }
