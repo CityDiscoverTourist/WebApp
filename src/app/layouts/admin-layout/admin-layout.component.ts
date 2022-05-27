bc7737eb3e6a355b54782547d0796d32fb5e5688
ssangnd <sangndxyz@gmail.com>
2022-05-27 17:05:20 +0700

    ui admin component

--- src/app/layouts/admin-layout/admin-layout.component.ts
+++ src/app/layouts/admin-layout/admin-layout.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  HostBinding,
+  Input,
+  OnInit,
+  Output,
+} from '@angular/core';
 
 @Component({
   selector: 'app-admin-layout',
   templateUrl: './admin-layout.component.html',
-  styleUrls: ['./admin-layout.component.scss']
+  styleUrls: ['./admin-layout.component.scss'],
 })
 export class AdminLayoutComponent implements OnInit {
+  ngOnInit(): void {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  status = false;
+  AddFunction() {
+    if (!this.status) {
+      this.status = true;
+    } else {
+      this.status = false;
+    }
   }
-
 }
