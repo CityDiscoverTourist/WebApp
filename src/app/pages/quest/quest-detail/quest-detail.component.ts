f39a0b83359e9cc2881d7cabaf81157408449587
ssangnd <sangndxyz@gmail.com>
2022-05-31 17:33:21 +0700

    ui and logic for quest detail

--- src/app/pages/quest/quest-detail/quest-detail.component.ts
+++ src/app/pages/quest/quest-detail/quest-detail.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { BsModalService } from 'ngx-bootstrap/modal';
 import { QuestListItem } from 'src/app/models';
 import { QuestService } from 'src/app/services';
+import { QuestDeleteModalComponent } from '../../share/quest-delete-modal/quest-delete-modal.component';
 
 @Component({
   selector: 'app-quest-detail',
@@ -13,18 +15,38 @@ export class QuestDetailComponent implements OnInit {
   public questListItem:QuestListItem;
   constructor(
     private route: ActivatedRoute,
-    private questService: QuestService
+    private questService: QuestService,
+    private modalService: BsModalService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
   ) {}
 
   ngOnInit(): void {
     // this.id = this.route.snapshot.paramMap.get('id');
     this.id = this.route.snapshot.params['id'];
-    console.log(this.id);
-    
+    // console.log(this.id);
     this.questService.getQuestById(this.id).subscribe((data) => {
      this.questListItem=data;
      console.log(data);
-     
     });
+  }
+
+  showDeleteQuest() {
+    const bsModalRef = this.modalService.show(QuestDeleteModalComponent,{
+      initialState: {
+        id: this.id,
+      },
+    });
+
+    // bsModalRef.onHide?.pipe(take(1)).subscribe({
+    //   next: (result) => {
+    //     const questypeIds = result as { id: number; name: string };
+    //     this.questypeIds$.next({ id: questypeIds.id, name: questypeIds.name });
+    //   },
+    // });
+  }
+
+  editQuest(){
+    this.router.navigate(['admin/quest/edit'],{queryParams:{id:this.id}});
   }
 }
--- src/app/pages/quest/quest-detail/quest-detail.module.ts
+++ src/app/pages/quest/quest-detail/quest-detail.module.ts
@@ -3,15 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { QuestDetailRoutingModule } from './quest-detail-routing.module';
 import { QuestDetailComponent } from './quest-detail.component';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
-
+const lib=[
+ ModalModule.forRoot()
+]
 @NgModule({
   declarations: [
     QuestDetailComponent
   ],
   imports: [
     CommonModule,
-    QuestDetailRoutingModule
+    QuestDetailRoutingModule,
+    ...lib
   ]
 })
 export class QuestDetailModule { }
