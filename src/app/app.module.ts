9dce2a5e0d6e5b5f4cd27c0a030fc38707536622
ssangnd <sangndxyz@gmail.com>
2022-06-01 17:26:18 +0700

    remove primeng lib

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ComponentsModule } from './components/components.module';
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 import { QuestOwnerLayoutComponent } from './layouts/quest-owner-layout/quest-owner-layout.component';
 import { QuestTypeComponent } from './pages/quest-type/quest-type.component';
 
 import { QuillModule } from 'ngx-quill';
 import { HttpClientModule } from '@angular/common/http';
-import { ValidationMessageComponent } from './modules/validation-message/validation-message.component';
-
-import {PanelModule} from 'primeng/panel';
-import {ButtonModule} from 'primeng/button';
-import {TableModule} from 'primeng/table';
-import {PaginatorModule} from 'primeng/paginator';
-import {BlockUIModule} from 'primeng/blockui';
-import {ProgressSpinnerModule} from 'primeng/progressspinner';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NotificationService } from './services';
 import { LocationTypeModalComponent } from './pages/share/location-type-modal/location-type-modal.component';
@@ -41,14 +32,8 @@ const lib=[
   ],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, BrowserAnimationsModule,
     ...lib,
-    PanelModule,
-    ButtonModule,
-    TableModule,
-    PaginatorModule,
-    BlockUIModule,
     FormsModule,
     ReactiveFormsModule,
-    ProgressSpinnerModule,
   ],
   providers:[
     NotificationService
