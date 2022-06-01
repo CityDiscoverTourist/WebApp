0d07faa2274f0ef6786ad5a1b70e1a79385926d3
ssangnd <sangndxyz@gmail.com>
2022-06-01 17:27:01 +0700

    add method getcities

--- src/app/services/city.service.ts
+++ src/app/services/city.service.ts
@@ -1,176 +1,32 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { id } from '@swimlane/ngx-datatable';
-import { catchError, map, Observable, of } from 'rxjs';
-import { environment } from 'src/environments/environment';
-import { IdValue, pagination } from '../models';
-import { City, CityForTest } from '../models/city.model';
-import { Pagination } from '../models/pagination.model';
-import { BaseService } from './base.service ';
+import { stringify } from 'query-string';
+import { Observable } from 'rxjs';
+import { City, CityListItem, Paging, SearchInfo } from '../models';
 
 @Injectable({
   providedIn: 'root',
 })
-export class CityService extends BaseService {
-  private _sharedHeaders = new HttpHeaders();
-  constructor(private http: HttpClient) {
-    super();
-    // this._sharedHeaders = this._sharedHeaders.set(
-    //   'Content-Type',
-    //   'application/json'
-    // );
-  }
-  add(entity: City) {
-    entity.status = 'true';
-    entity.id = '0';
-    var k = JSON.stringify(entity);
-    console.log('data nè ' + k);
-    var kkk = { id: 0, name: 'jjj', status: 'kkkdjdh' };
-    var ddhhshau = JSON.stringify(kkk);
-    var dhsahj = JSON.stringify(entity);
-    var input = { id: 0, name: entity.name, status: entity.status };
-    var inputStringify = JSON.stringify(input);
-    var hsahjash = 9989;
-    return (
-      this.http
-        // .post(`${environment.apiUrl}/citys`, JSON.stringify(kkk), {
-        .post(
-          `https://localhost:7235/api/v1/citys`,
-          JSON.stringify(input),
-          this.httpOptions
-        )
-    );
-    // .pipe(catchError(this.handleError));
-  }
-
-  update(id: string, entity: City) {
-    var kkk = { id: 0, name: 'jjj', status: 'kkkdjdh' };
-
-    return this.http.put(
-      `https://localhost:7235/api/v1/citys/`,
-      JSON.stringify(entity),
-      this.httpOptions
-    );
-    // .pipe(catchError(this.handleError));
-  }
-
-  getDetail(id: string) {
-    return this.http.get<Pagination<City>>(
-      `https://localhost:7235/api/v1/citys/${id}`,
-      this.httpOptions
-    );
-    // .pipe(catchError(this.handleError));
-  }
-  //   getDetail(id:number) {
-  //     return this.http.get<City>(`${environment.apiUrl}/api/cities/${id}`, { headers: this._sharedHeaders })
-  //         .pipe(catchError(this.handleError));
-  // }
+export class CityService {
+  constructor(private http: HttpClient) {}
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
   };
-  getAllPaging(
-    nameFiler: string,
-    pageNumber: number,
-    pageSize: number,
-    orderBy: string
-  ) {
-    return this.http.get<Pagination<City>>(
-      `https://localhost:7235/api/v1/citys?Name=${nameFiler}&PageNumber=${pageNumber}&PageSize=${pageSize}&OrderBy=${orderBy}`,
+  getCities(search: SearchInfo): Observable<Paging<City>> {
+    var sortBy =
+      `${search.sort?.sortBy}` === 'undefined' ? '' : search.sort?.sortBy;
+    var sortDir =
+      `${search?.sort?.dir}` === 'undefined' ? '' : search.sort?.dir;
+    const query = stringify({
+      name: search.keyword,
+      pageNumber: isNaN(search?.currentPage!) ? 1 : search?.currentPage! + 1,
+      pagesize: 10,
+      orderby: `${sortBy} ${sortDir}`,
+    });
+    var result = this.http.get<Paging<City>>(
+      'https://citytourist.azurewebsites.net/api/v1/cites?' + query,
       this.httpOptions
     );
-  }
-
-  delete(id: string) {
-    return this.http
-      .delete('https://localhost:7235/api/v1/citys/' + id, this.httpOptions)
-      .pipe(catchError(this.handleError));
-  }
-  // private _sharedHeaders = new HttpHeaders();
-  // constructor(private http: HttpClient) {
-  //     super();
-  //     this._sharedHeaders = this._sharedHeaders.set('Content-Type', 'application/json');
-  // }
-  // add(entity: City) {
-  //     return this.http.post(`${environment.apiUrl}/api/cities`, JSON.stringify(entity), { headers: this._sharedHeaders })
-  //         .pipe(catchError(this.handleError));
-  // }
-
-  // update(id: string, entity: City) {
-  //     return this.http.put(`${environment.apiUrl}/api/cities/${id}`, JSON.stringify(entity), { headers: this._sharedHeaders })
-  //         .pipe(catchError(this.handleError));
-  // }
-
-  // getAllPaging(filter:string, pageIndex:number, pageSize:number) {
-  //     return this.http.get<Pagination<City>>(`${environment.apiUrl}/api/roles/filter?pageIndex=${pageIndex}&pageSize=${pageSize}&filter=${filter}`, { headers: this._sharedHeaders })
-  //         .pipe(map((response: Pagination<City>) => {
-  //             return response;
-  //         }), catchError(this.handleError));
-  // }
-
-  // delete(id:number) {
-  //     return this.http.delete(environment.apiUrl + '/api/roles/' + id, { headers: this._sharedHeaders })
-  //         .pipe(
-  //             catchError(this.handleError)
-  //         );
-  // }
-
-  // getCityType():Observable<IdValue[]>{
-  //   return of([...Array(6).keys()].map(
-  //     (i) =>
-  //       ({
-  //         id: i,
-  //         value: `${i} id`
-  //       })));
-  // }
-
-  // getAll(){
-  //   return this.http.get("https://citytourist.azurewebsites.net/api/v1/citys",this.httpOptions).pipe(map((res: any) => Object.values(res)))
-  //   ngOnInit(): void {
-  //  var result = this.s.getAll();
-  //  console.log('data ' + result?.forEach((x) => console.log('x x' + x)));
-  //  console.log('data ' + result);
-
-  //  var b = 1;
-
-  getCityType(): Observable<IdValue[]> {
-    // return of([...Array(6).keys()].map(
-    //   (i) =>
-    //     ({
-    //       id: i,
-    //       value: `${i} id`
-    //     })));
-    const data: IdValue[] = [];
-    // var result =
-    var result= this.http
-      .get<Pagination<CityForTest>>(
-        'https://citytourist.azurewebsites.net/api/v1/cites',
-        this.httpOptions
-      )
-      .pipe(
-        map((response: Pagination<CityForTest>) =>
-          // res.data.forEach(x=>console.log("dddd", x.id + x.name +x.status))
-          //return of()
-          [...response.data].map(
-            (i) =>
-              ({
-                id: i.id,
-                value: `${i.name}`,
-              } as IdValue)
-          )
-        )
-      );
-
-    console.log(result);
-    var a=0;
-    
     return result;
-      
-    // return of(
-    //   [...Array(6).keys()].map((i) => ({
-    //     id: i,
-    //     value: `${i} id`,
-    //   }))
-    // );
-    // return result;
   }
 }
