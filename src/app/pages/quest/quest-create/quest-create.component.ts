ec2163352ab63d2810b651ed71ffcedda61d4a72
ssangnd <sangndxyz@gmail.com>
2022-06-02 21:27:33 +0700

    update ui and logic for create quest

--- src/app/pages/quest/quest-create/quest-create.component.ts
+++ src/app/pages/quest/quest-create/quest-create.component.ts
@@ -17,7 +17,7 @@ import {
 } from 'rxjs';
 import { RxState } from '@rx-angular/state';
 import { QuestState, QUEST_STATE } from '../states/quest.state';
-import { IdValue, QuestData } from 'src/app/models';
+import { IdValue, QuestCreate } from 'src/app/models';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { HotToastService } from '@ngneat/hot-toast';
 import { QuestService } from 'src/app/services';
@@ -66,12 +66,8 @@ export class QuestCreateComponent implements OnInit {
     this.state.connect(
       this.selectedFile$
         .pipe(tap(() => setTimeout(() => this.cd.detectChanges(), 100)))
-        //pick image event
         .pipe(tap((file) => this.form.patchValue({ image: file[0] }))),
-      (_prev, files) => ({
-        // files: [...prev.files, ...files],
-        files: [...files],
-      })
+      (_prev, files) => ({ files: [...files] })
     );
 
     this.state.connect(
@@ -87,53 +83,46 @@ export class QuestCreateComponent implements OnInit {
     );
 
     //cach này đúng nhưng dỡ
-    this.state.connect(
-      this.formSubmit$.pipe(
-        switchMap((f) => this.questService.addQuest(f.value as QuestData))
-      ).pipe(tap((data)=>{
-        console.log(data);
-        console.log("có vao ko");
-        
-      })),
-      (_prev, curr) => ({
-        error: undefined,
-      })
-    );
-    //hay
-    // const [valid$,invalid$]=partition(
-    //   this.submit$,
-    //   f=>f.valid
+    // this.state.connect(
+    //   this.formSubmit$
+    //     .pipe(
+    //       switchMap((f) => this.questService.addQuest(f.value as QuestCreate))
+    //     )
+    //     .pipe(
+    //       tap((data) => {
+    //         console.log(data);
+    //         console.log('có vao ko');
+    //       })
+    //     ),
+    //   (_prev, curr) => ({
+    //     error: undefined,
+    //   })
     // );
 
-    // this.state.connect(
-    //   valid$.pipe(
-    //     //them loading bo neu duoc
-    //     tap(()=>this.state.set({submitting:true})),
-    //     switchMap((f)=>this.questService.addQuest(f.value as QuestData)
-    //     // .pipe(catchError((err))=>of(err))
-    //     ),
-    //     tap(result=>{
-    //       if(result.status)//if(result.isOk)
-    //       {
-    //         this.toast.success("Tao product thanh cong")
-    //       }
-    //     })
-    //   ),(_prev,curr)=>({
-    //     // error:undefined,
-    //     error:curr.status,
-    //     submitting:false
-    //   })
-    // )
-    //hay
+    const [valid$, invalid$] = partition(this.submit$, (f) => f.valid);
 
-    // this.state.hold(
-    //   invalid$.pipe(
-    //   ),f=>{
-    //     this.toast.error('Loi roi ne');
-    //     // this.form.revalidateControls([]);
-    //     f.revalidateControls([]);
-    //   }
-    // )
+    this.state.connect(
+      valid$.pipe(
+        tap(() => this.state.set({ submitting: true })),
+        switchMap((f) => this.questService.addQuest(f.value as QuestCreate)),
+        tap((result) => {
+          if (result.id) {
+            this.toast.success('Tạo quest thành công');
+          }
+        })
+      ),
+      (_prev, curr) => ({
+      
+        error: undefined,
+        submitting: false,
+      })
+    );
+    // hay
+    this.state.hold(invalid$.pipe(), (f) => {
+      this.toast.error('Giá trị bạn nhập không đúng');
+      // this.form.revalidateControls([]);
+      f.revalidateControls([]);
+    });
 
     this.questState.connect(this.questypeIds$, (prev, curr) => ({
       questTypeIds: [...prev.questTypeIds, { id: curr.id, value: curr.name }],
@@ -147,12 +136,10 @@ export class QuestCreateComponent implements OnInit {
 
   get questTypeIds(): Observable<IdValue[]> {
     return this.questState.select('questTypeIds');
-    // .pipe(tap((m) => console.log(m)));
   }
 
   get areaIds(): Observable<IdValue[]> {
     return this.questState.select('areaIds');
-    // .pipe(tap((m) => console.log(m)));
   }
 
   form!: FormGroup;
@@ -161,19 +148,14 @@ export class QuestCreateComponent implements OnInit {
     this.form = this.fb.group({
       id: [0],
       title: ['', [Validators.required, Validators.minLength(8)]],
-      // title: [],
-      description: [],
-      // description: ['',[Validators.required, Validators.minLength(10)]],
+      description: [''],
       price: [0, [Validators.required, Validators.min(10)]],
-      // price: [],
-      // estimatedTime: ['', [customQuestValidator()]],
       estimatedTime: [''],
-      estimatedDistance: [],
+      estimatedDistance: [''],
       availableTime: [],
-      // availableTime: ['', [Validators.pattern("^(1[0123456789]|[1-9])(am|pm)\-(1[012]|[1-9])(am|pm)")]],
       questTypeId: [],
       image: [],
-      questOwnerId: [2],
+      questOwnerId: [''],
       areaId: [],
       status: [false],
     });
@@ -181,54 +163,25 @@ export class QuestCreateComponent implements OnInit {
   submitForm() {
     const valid = this.form.valid;
     this.formSubmit$.next(this.form);
-    //if
-    console.log(`form state =${valid}`, this.form.value, this.form.errors);
-    // this.formSubmit$.next(this.form);
+    console.log(`form state =${valid}`, this.form.value);
 
     if (valid) {
-      this.toast.success('Ok roi ne');
       this.formSubmit$.next(this.form);
     } else {
-      this.toast.error('Loi roi ne');
-      // this.form.get('title')?.markAsTouched({onlySelf:true});
-      // this.form.get('price')?.markAsTouched({onlySelf:true});
-      // for(let key in this.form.controls){
-      //   this.form.get(key)?.markAsTouched({onlySelf:true});
-      // }
       this.form.revalidateControls([]);
     }
   }
   selectedFile$ = new Subject<File[]>();
   removedFiles$ = new Subject<File>();
-
-  // hasError(key:string):boolean{
-  //   const control=this.form.get(key);
-  //   if(!control){
-  //     return false;
-  //   }
-  //   return control.invalid && (control.dirty || control.touched);
-  // }
-
-  // formSubmit$ = new Subject<FormGroup>();
   formSubmit$ = new Subject<FormGroup>();
   submit$ = new Subject<FormGroup>();
 
-  // onFileSelect(event: Event) {
-  //   const input = event.target as HTMLInputElement;
-  //   if (!input.files?.length) {
-  //     return;
-  //   }
-  //   const file = input.files[0];
-  //   this.form.patchValue({ image: file });
-  // }
-  
   showAddQuestType() {
     const bsModalRef = this.modalService.show(QuestTypeModalComponent, {
       initialState: {
         simpleForm: true,
       },
     });
-
     bsModalRef.onHide?.pipe(take(1)).subscribe({
       next: (result) => {
         const questypeIds = result as { id: number; name: string };
