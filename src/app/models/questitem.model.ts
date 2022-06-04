b4f73509b2c3cee4845197c9a3c2a58bc7981972
ssangnd <sangndxyz@gmail.com>
2022-06-04 14:08:25 +0700

    add questitem, questitemtype model

---
+++ src/app/models/questitem.model.ts
@@ -0,0 +1,58 @@
+export interface QuestItem {
+  id: number;
+  content: string;
+  description: string;
+  duration: number;
+  createdDate: Date;
+  updatedDate: Date;
+  qrCode: string;
+  triggerMode: number;
+  rightAnswer: string;
+  answerImageUrl: string;
+  status: string;
+  questItemTypeId: number;
+  locationId: number;
+  questId: number;
+  itemId: number;
+}
+
+export interface QuestItemCreate{
+  id: number;
+  content: string;
+  description: string;
+  duration: number;
+  createdDate: Date;
+  updatedDate: Date;
+  qrCode: string;
+  triggerMode: number;
+  rightAnswer: string;
+  answerImageUrl: string;
+  status: string;
+  questItemTypeId: number;
+  locationId: number;
+  questId: number;
+  itemId: number;
+}
+
+export interface QuestItemListItem {
+  index: number;
+  id: number;
+  content: string;
+  description: string;
+  duration: number;
+  createdDate: Date;
+  updatedDate: Date;
+  qrCode: string;
+  triggerMode: number;
+  rightAnswer: string;
+  answerImageUrl: string;
+  status: string;
+  questItemTypeId: number;
+  locationId: number;
+  questId: number;
+  itemId: number;
+}
+
+export interface QuestItemCreateResult{
+  id:number;
+}
