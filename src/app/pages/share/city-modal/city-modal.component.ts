e33d5a725c79b3284e03df779216e315488b647a
ssangnd <sangndxyz@gmail.com>
2022-06-05 20:18:17 +0700

    add process logic city modal

---
+++ src/app/pages/share/city-modal/city-modal.component.ts
@@ -0,0 +1,89 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { RxState } from '@rx-angular/state';
+import { BsModalRef } from 'ngx-bootstrap/modal';
+import {
+  catchError,
+  filter,
+  Observable,
+  of,
+  partition,
+  Subject,
+  switchMap,
+  tap,
+} from 'rxjs';
+import { CityService } from 'src/app/services';
+
+declare type ModalState = {
+  hasError: boolean;
+};
+
+@Component({
+  selector: 'app-city-modal',
+  templateUrl: './city-modal.component.html',
+  styleUrls: ['./city-modal.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [RxState],
+})
+export class CityModalComponent implements OnInit {
+  constructor(
+    public bsModalRef: BsModalRef,
+    private fb: FormBuilder,
+    private state: RxState<ModalState>,
+    private cityService: CityService
+  ) {}
+
+  ngOnInit(): void {
+    this.initForm();
+    const [$valid, $invalid] = partition(this.submit$, (f) => f.valid);
+    this.state.connect(
+      $valid
+        .pipe(
+          switchMap((form) =>
+            this.cityService
+              .addCity(form.value)
+              .pipe(
+                catchError(() =>
+                  of({ status: 'data not modified', data: null })
+                )
+              )
+          )
+        )
+        .pipe(
+          filter((result) => (result.status == 'data modified' ? true : false)),
+          tap((result) => {
+            this.bsModalRef.onHide?.emit({
+              id: result?.data?.id,
+              name: result?.data?.name,
+            });
+            this.bsModalRef.hide();
+          })
+        ),
+      (_prev, curr) => ({
+        hasError: curr.status == 'data modified' ? false : true,
+      })
+    );
+    this.state.connect(
+      $invalid.pipe(tap(() => this.form.revalidateControls([]))),
+      () => ({
+        hasError: true,
+      })
+    );
+  }
+
+  form!: FormGroup;
+  simpleForm = false;
+
+  initForm() {
+    this.form = this.fb.group({
+      name: [null, [Validators.required]],
+      status: ['Không hiển thị'],
+    });
+  }
+
+  submit$ = new Subject<FormGroup>();
+
+  public get hasError$(): Observable<boolean> {
+    return this.state.select('hasError');
+  }
+}
