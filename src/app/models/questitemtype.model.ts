64a175293fdfe757f464cf54eb59262899b0a3aa
ssangnd <sangndxyz@gmail.com>
2022-06-06 17:21:54 +0700

    add QuestItemType model

--- src/app/models/questitemtype.model.ts
+++ src/app/models/questitemtype.model.ts
@@ -4,9 +4,20 @@ export interface QuestItemType{
     status:string;
 }
 
+export interface QuestItemTypeCreate{
+    id:number;
+    name:string;
+    status:string;
+}
+
 export interface QuestItemTypeListItem{
     index:number;
     id:number;
     name:string;
     status:string;
+}
+
+
+export interface QuestItemTypeCreateResult{
+    id:number;
 }
