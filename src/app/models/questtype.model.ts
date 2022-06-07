cb78f4f1ac394a712967249bf9755f0c7859aa18
ssangnd <sangndxyz@gmail.com>
2022-06-07 21:42:00 +0700

    modify questtype model

--- src/app/models/questtype.model.ts
+++ src/app/models/questtype.model.ts
@@ -2,17 +2,20 @@ export interface QuestType{
   id:number;
   name:string;
   status:string;
+  imagePath:string;
 }
 
 export interface QuestTypeCreate{
   id:number;
   name:string;
   status:string;
+  image:File;
 }
 export interface QuestTypeListItem{
   index:number;
   id:number;
   name:string;
   status:string;
+  imagePath:string;
 }
 
