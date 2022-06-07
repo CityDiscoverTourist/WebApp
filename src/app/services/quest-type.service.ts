f1a372e1355b5692fdb7809a5d5ad5d5c82317b7
ssangnd <sangndxyz@gmail.com>
2022-06-07 21:42:37 +0700

    add Func crud for questtype

--- src/app/services/quest-type.service.ts
+++ src/app/services/quest-type.service.ts
@@ -1,10 +1,11 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { stringify } from 'query-string';
 import { map, of } from 'rxjs';
 import { Observable } from 'rxjs/internal/Observable';
-import { IdValue, Result } from '../models';
+import { IdValue, Paging, Result, SearchInfo } from '../models';
 import { Pagination } from '../models/pagination.model';
-import { QuestType } from '../models/questtype.model';
+import { QuestType, QuestTypeCreate, QuestTypeListItem } from '../models/questtype.model';
 
 @Injectable({
   providedIn: 'root'
@@ -34,16 +35,71 @@ export class QuestTypeService {
         )
       );
   }
-  addQuestType(questType:Partial<QuestType>): Observable<Result<Partial<QuestType>>> {
-    // return of({
-    //   message:'',
-    //   data: questType,
-    //   status: 'data modified',
-    // });
+  
+  getQuestTypes(
+    search: SearchInfo
+  ): Observable<Paging<QuestTypeListItem>> {
+    var sortBy =
+      `${search.sort?.sortBy}` === 'undefined' ? '' : search.sort?.sortBy;
+    var sortDir =
+      `${search?.sort?.dir}` === 'undefined' ? '' : search.sort?.dir;
+    const query = stringify({
+      name: search.keyword,
+      pageNumber: isNaN(search?.currentPage!) ? 1 : search?.currentPage! + 1,
+      pagesize: 10,
+      orderby: `${sortBy} ${sortDir}`,
+    });
+    var result = this.http.get<Paging<QuestTypeListItem>>(
+      'https://citytourist.azurewebsites.net/api/v1/quest-types?' + query,
+      this.httpOptions
+    );
+    return result;
+  }
+
+  addQuestType(
+    payload: Partial<QuestTypeCreate>
+  ): Observable<Result<Partial<QuestType>>> {
     return this.http.post<Result<Partial<QuestType>>>(
       `https://citytourist.azurewebsites.net/api/v1/quest-types/`,
-      questType,
+      payload,
       this.httpOptions
     );
   }
+  deleteQuestTypeById(id: string): Observable<string | undefined> {
+    return this.http
+      .delete(
+        `https://citytourist.azurewebsites.net/api/v1/quest-types/${id}`,
+        this.httpOptions
+      )
+      .pipe(map((response: Result<QuestTypeListItem>) => response.status));
+  }
+
+  getQuestTypeById(id: string | undefined): Observable<QuestType> {
+    return this.http
+      .get<Result<QuestType>>(
+        `https://citytourist.azurewebsites.net/api/v1/quest-types/${id}`,
+        this.httpOptions
+      )
+      .pipe(
+        map(
+          (response: Result<QuestType>) =>
+            ({
+              id: response.data?.id,
+              name: response.data?.name,
+              status: response.data?.status,
+            } as QuestType)
+        )
+      );
+  }
+  updateQuestTypeById(
+    payload: Partial<QuestTypeCreate>
+  ): Observable<Result<Partial<QuestType>>> {
+    return this.http.put<Result<Partial<QuestType>>>(
+      `https://citytourist.azurewebsites.net/api/v1/quest-types/`,
+      payload,
+      this.httpOptions
+    );
+  }
+
 }
+
