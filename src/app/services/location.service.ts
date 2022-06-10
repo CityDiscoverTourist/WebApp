83f7f9e090971f605821dd5aaec5985811145301
ssangnd <sangndxyz@gmail.com>
2022-06-10 20:10:51 +0700

    add Func getLocations

--- src/app/services/location.service.ts
+++ src/app/services/location.service.ts
@@ -13,6 +13,7 @@ import {
 } from '../models';
 import { Pagination } from '../models/pagination.model';
 import { Location } from '../models';
+import { stringify } from 'query-string';
 
 @Injectable({
   providedIn: 'root',
@@ -40,20 +41,36 @@ export class LocationService {
           )
         )
       );
-    var data = result.subscribe((data) => {
-      console.log(data);
-    });
     return result;
   }
-  addLocation(
-    payload: LocationCreate
-  ): Observable<LocationCreateResult> {
+
+  getLocations(
+    search: LocationListSearch
+  ): Observable<Paging<LocationListItem>> {
+    var sortBy =
+      `${search.sort?.sortBy}` === 'undefined' ? '' : search.sort?.sortBy;
+    var sortDir =
+      `${search?.sort?.dir}` === 'undefined' ? '' : search.sort?.dir;
+    const query = stringify({
+      name: search.keyword,
+      areaId: search.areaIds,
+      locationTypeId: search.locationTypeIds,
+      pageNumber: isNaN(search?.currentPage!) ? 1 : search?.currentPage! + 1,
+      pagesize: 10,
+      orderby: `${sortBy} ${sortDir}`,
+    });
+    var result = this.http.get<Paging<LocationListItem>>(
+      `https://citytourist.azurewebsites.net/api/v1/locations?` + query,
+      this.httpOptions
+    );
+    return result;
+  }
+
+  addLocation(payload: LocationCreate): Observable<LocationCreateResult> {
     return this.http.post<LocationCreateResult>(
       `https://citytourist.azurewebsites.net/api/v1/locations`,
       payload,
       this.httpOptions
     );
   }
-
-  
 }
