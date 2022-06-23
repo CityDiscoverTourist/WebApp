7e7105bf22eedb6b12f7f353d5a72086f8296531
ssangnd <sangndxyz@gmail.com>
2022-06-23 10:11:46 +0700

    add storageService

--- src/app/services/index.ts
+++ src/app/services/index.ts
@@ -1,3 +1,6 @@
+export * from "./authenticate.service";
+export * from "./storage.service";
+export * from "./user.service";
 export * from "./quest.service";
 export * from "./notification.service";
 export * from "./city.service";
