ef84cebe2f57bb24395293ad04bf6d5d65214dbb
ssangnd <75923274+ssangnd@users.noreply.github.com>
2022-07-05 21:54:22 +0700

    modify ngxtable-type

--- src/app/types/ngxdatatable.type.d.ts
+++ src/app/types/ngxdatatable.type.d.ts
@@ -1,14 +1,3 @@
-// export declare type PagingInfo = {
-//     count:number;
-//     pageSize:number;
-//     limit:number;
-//     offset:number;
-//   }
-//   export declare type SortInfo ={
-//     column:{prop:string};
-//     newValue:'asc'|'desc'
-//   }
-
 export declare type PageInfo = {
     count:number;
     pageSize:number;
