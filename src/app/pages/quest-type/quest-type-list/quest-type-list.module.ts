0d1f2790801c5b13eb33e89056a03fa4b95175e5
ssangnd <75923274+ssangnd@users.noreply.github.com>
2022-07-08 12:33:01 +0700

    add filter success

--- src/app/pages/quest-type/quest-type-list/quest-type-list.module.ts
+++ src/app/pages/quest-type/quest-type-list/quest-type-list.module.ts
@@ -1,15 +1,14 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
 import { QuestTypeListRoutingModule } from './quest-type-list-routing.module';
 import { QuestTypeListComponent } from './quest-type-list.component';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { NgxDatatableModule } from '@swimlane/ngx-datatable';
-import { HotToastModule } from '@ngneat/hot-toast';
-import { ModalModule } from 'ngx-bootstrap/modal';
 import { NgSelectModule } from '@ng-select/ng-select';
+import { HotToastModule } from '@ngneat/hot-toast';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 const lib = [
   NgxDatatableModule,
