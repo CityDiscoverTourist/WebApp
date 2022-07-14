d7e24b6b2504b2bebeb49bb1d3c196fa1b717890
ssangnd <75923274+ssangnd@users.noreply.github.com>
2022-07-14 22:41:58 +0700

    add modal isBlock customer process logic

--- src/app/pages/share/index.ts
+++ src/app/pages/share/index.ts
@@ -3,3 +3,4 @@ export  * from './area-modal/area-modal.component';
 export  * from './quest-type-modal/quest-type-modal.component';
 export  * from './location-modal/location-modal.component';
 export  * from './location-type-modal/location-type-modal.component';
+export  * from './is-block-customer-modal/is-block-customer-modal.component';
---
+++ src/app/pages/share/is-block-customer-modal/is-block-customer-modal.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { HotToastService } from '@ngneat/hot-toast';
+import { BsModalRef } from 'ngx-bootstrap/modal';
+import { CustomerService } from 'src/app/services';
+
+@Component({
+  selector: 'app-is-block-customer-modal',
+  templateUrl: './is-block-customer-modal.component.html',
+  styleUrls: ['./is-block-customer-modal.component.scss'],
+})
+export class IsBlockCustomerModalComponent implements OnInit {
+  id: string = '';
+  title: string = '';
+  status: boolean = false;
+  mail: string = '';
+  constructor(
+    public bsModalRef: BsModalRef,
+    private toast: HotToastService,
+    private customerService: CustomerService
+  ) {}
+
+  ngOnInit(): void {}
+
+  isBlock(id: string) {
+    this.customerService.isBlockCustomer(id, this.status).subscribe((data) => {
+      this.bsModalRef.onHide?.emit({
+        data: data,
+      });
+      this.bsModalRef.hide();
+      if (this.status) {
+        this.toast.success(`Block ${this.title} thành công`);
+      } else {
+        this.toast.success(`Unblock ${this.title} thành công`);
+      }
+    });
+  }
+}
