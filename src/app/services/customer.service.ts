93f8e90ff410f0514eb8fdcdadee5601f75091ec
ssangnd <75923274+ssangnd@users.noreply.github.com>
2022-07-21 18:26:47 +0700

    modify env

--- src/app/services/customer.service.ts
+++ src/app/services/customer.service.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { stringify } from 'query-string';
 import { map, Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
+import { environment } from 'src/environments/environment.prod';
 import { Customer, CustomerListSearch, Paging, Result } from '../models';
 import { BaseService } from './base.service';
 
