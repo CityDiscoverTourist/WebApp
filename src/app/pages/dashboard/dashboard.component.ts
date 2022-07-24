03465ca6d16a0e0e0fdee4d74fc77c567147f85b
ssangnd <75923274+ssangnd@users.noreply.github.com>
2022-07-24 23:44:29 +0700

    modify dashboard

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -24,10 +24,10 @@ export class DashboardComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.dashboardService.getTotalRevenue().subscribe(data=>this.totalValue=data);
-    this.dashboardService.getTotalQuest().subscribe(data=>this.totalQuest=data);
     this.dashboardService.getTotalUser().subscribe(data=>this.totalUser=data);
     this.dashboardService.getTopPlayer().subscribe(data=>this.listTopUserPoint=data);
+    this.dashboardService.getTotalRevenue().subscribe(data=>this.totalValue=data);
+    this.dashboardService.getTotalQuest().subscribe(data=>this.totalQuest=data);
     this.dashboardService.getTopQuestPlay().subscribe(data=>this.listTopQuest=data);
   }
 }
