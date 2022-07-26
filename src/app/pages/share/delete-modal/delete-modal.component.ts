a8c0f5440b211c0bfc6df47889d2ffcb2c976c2e
ssangnd <75923274+ssangnd@users.noreply.github.com>
2022-07-26 09:57:04 +0700

    modify for enable disable quest type

--- src/app/pages/share/delete-modal/delete-modal.component.ts
+++ src/app/pages/share/delete-modal/delete-modal.component.ts
@@ -38,25 +38,44 @@ export class DeleteModalComponent implements OnInit {
   ngOnInit(): void {}
   updateStatus(id: string, status: string) {
     switch (this.title) {
-      case 'loại Quest Item':
+      case 'loại câu hỏi':
         {
-          this.questItemTypeService
-            .deleteQuestItemTypeById(id)
-            .subscribe((data) => {
+          this.questItemTypeService.updateStatus(id, status).subscribe((data) => {
+            try {
               if (data?.questItems?.length) {
                 this.bsModalRef.hide();
-                this.toast.error(`Xóa ${this.title} không thành công`);
-                this.toast.info(
-                  `Loại quest item này đang chứa các quest item khác nên không xóa được`
+                this.toast.error(
+                  `${
+                    status == 'Active'
+                      ? 'Hoạt động lại'
+                      : 'Không thể ngừng hoạt động'
+                  } ${this.title} ${data.name}!
+                  <br> 
+                  Loại câu hỏi này đang chứa câu hỏi khác nên không thể ngừng hoạt động!
+                `,
+                  {
+                    autoClose: false,
+                    dismissible: true,
+                  }
                 );
               } else {
                 this.bsModalRef.onHide?.emit({
                   data: data,
                 });
                 this.bsModalRef.hide();
-                this.toast.success(`Xóa ${this.title} thành công`);
+                this.toast.success(
+                  `${
+                    status == 'Active' ? 'Hoạt động lại' : 'Ngừng hoạt động'
+                  } ${this.title} ${data?.name} thành công!`,
+                  {
+                    duration: 5000,
+                  }
+                );
               }
-            });
+            } catch (error) {
+              this.toast.error('Có lỗi hãy kiểm tra lại!');
+            }
+          });
         }
         break;
       case 'thành phố':
@@ -224,21 +243,42 @@ export class DeleteModalComponent implements OnInit {
           });
         }
         break;
-      case 'loại Quest':
+      case 'loại quest':
         {
-          this.questTypeService.deleteQuestTypeById(id).subscribe((data) => {
-            if (data?.quests?.length) {
-              this.bsModalRef.hide();
-              this.toast.error(`Xóa ${this.title} không thành công`);
-              this.toast.info(
-                `Loại quest này đang chứa các quest khác nên không xóa được`
-              );
-            } else {
-              this.bsModalRef.onHide?.emit({
-                data: data,
-              });
-              this.bsModalRef.hide();
-              this.toast.success(`Xóa ${this.title} thành công`);
+          this.questTypeService.updateStatus(id, status).subscribe((data) => {
+            try {
+              if (data?.quests?.length) {
+                this.bsModalRef.hide();
+                this.toast.error(
+                  `${
+                    status == 'Active'
+                      ? 'Hoạt động lại'
+                      : 'Không thể ngừng hoạt động'
+                  } loại quest!
+                  <br> 
+                  Loại quest đang chứa các quest nên không thể ngừng hoạt động!
+                `,
+                  {
+                    autoClose: false,
+                    dismissible: true,
+                  }
+                );
+              } else {
+                this.bsModalRef.onHide?.emit({
+                  data: data,
+                });
+                this.bsModalRef.hide();
+                this.toast.success(
+                  `${
+                    status == 'Active' ? 'Hoạt động lại' : 'Ngừng hoạt động'
+                  } loại quest thành công!`,
+                  {
+                    duration: 5000,
+                  }
+                );
+              }
+            } catch (error) {
+              this.toast.error('Có lỗi hãy kiểm tra lại!');
             }
           });
         }
