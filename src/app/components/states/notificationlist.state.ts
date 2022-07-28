90a438a3f88ef6fc0c29074e641628a9dd61ebe6
ssangnd <75923274+ssangnd@users.noreply.github.com>
2022-07-28 13:09:40 +0700

    add notification model

---
+++ src/app/components/states/notificationlist.state.ts
@@ -0,0 +1,12 @@
+import { InjectionToken } from '@angular/core';
+import { RxState } from '@rx-angular/state';
+import {Notification,  PagingMetadata } from 'src/app/models';
+
+export interface NotificationListState {
+  notifications: Notification[];
+  metadata: PagingMetadata;
+  loading: boolean;
+}
+export const NOTIFICATION_LIST_STATE = new InjectionToken<
+  RxState<NotificationListState>
+>('NOTIFICATION_LIST_STATE');
