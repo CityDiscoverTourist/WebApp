b66317ea74109fc29e1e04615579055f3a45077b
ssangnd <75923274+ssangnd@users.noreply.github.com>
2022-07-30 00:35:14 +0700

    process logic render ui

--- src/app/pages/quest/quest-detail/comment/commemt-list/commemt-list.component.ts
+++ src/app/pages/quest/quest-detail/comment/commemt-list/commemt-list.component.ts
@@ -1,15 +1,176 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  TemplateRef,
+  ViewChild,
+  ViewEncapsulation,
+} from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { RxState } from '@rx-angular/state';
+import { DatatableComponent, TableColumn } from '@swimlane/ngx-datatable';
+import { BehaviorSubject, Observable, Subject, tap, switchMap } from 'rxjs';
+import { PagingMetadata, SearchInfo, Comment } from 'src/app/models';
+import { CommentService } from 'src/app/services';
+import { PageInfo, SortInfo } from 'src/app/types';
+import { CommentListState } from '../states';
 
 @Component({
   selector: 'app-commemt-list',
   templateUrl: './commemt-list.component.html',
-  styleUrls: ['./commemt-list.component.scss']
+  styleUrls: ['./commemt-list.component.scss'],
+  providers: [RxState],
+  encapsulation: ViewEncapsulation.None,
 })
 export class CommemtListComponent implements OnInit {
-
-  constructor() { }
+  constructor(
+    private commentListState: RxState<CommentListState>,
+    private commentService: CommentService
+  ) {}
 
   ngOnInit(): void {
+    //Comment
+    this.initTable();
+    this.commentListState.connect(
+      this.searchComment$.pipe(
+        tap(() => this.commentListState.set({ loading: true })),
+        switchMap((s) => this.commentService.getComment(s))
+      ),
+      (_, result) => ({
+        comments: result.data,
+        metadata: { ...result.pagination },
+        loading: false,
+      })
+    );
+
+    this.status = [
+      { id: 'Đã duyệt', value: 'true' },
+      { id: 'Không được duyệt', value: 'false' },
+    ];
+
+    this.commentListState.hold(this.submitSearchComment$, (form) => {
+      this.searchComment$.next({
+        ...this.searchComment$.getValue(),
+        ...form,
+        currentPage: 0,
+      }),
+        (this.table.offset = 0);
+    });
+
+    this.commentListState.connect(this.resetSearchComment$, (prev, _) => ({
+      metadata: { ...prev.metadata, currentPage: 0 },
+    }));
+
+    this.commentListState.hold(this.resetSearchComment$, () => {
+      this.searchFormComment.reset();
+      this.searchComment$.next({ currentPage: 0 });
+      this.table.offset = 0;
+    });
+  }
+  @ViewChild('colCreatedAt', { static: true }) colCreatedAt!: TemplateRef<any>;
+  @ViewChild('actionTemplate', { static: true })
+  public actionTemplate: TemplateRef<any>;
+  //Comment
+  initTable() {
+    this.columns = [
+      {
+        prop: 'name',
+        name: 'Khách hàng',
+        sortable: true,
+        canAutoResize: true,
+        width: 150,
+      },
+      {
+        prop: 'feedBack',
+        name: 'Nội dung phản hồi',
+        sortable: true,
+        canAutoResize: true,
+        width: 300,
+      },
+      {
+        prop: 'rating',
+        name: 'Đánh giá',
+        sortable: true,
+        canAutoResize: true,
+        width: 60,
+        headerClass: 'd-flex justify-content-center',
+        cellClass: 'd-flex justify-content-center',
+      },
+      {
+        prop: 'createdDate',
+        name: 'Ngày tạo',
+        sortable: true,
+        cellTemplate: this.colCreatedAt,
+        canAutoResize: true,
+        headerClass: 'd-flex justify-content-center',
+        cellClass: 'd-flex justify-content-center',
+        width: 60,
+      },
+      {
+        prop: 'isFeedbackApproved',
+        name: 'Trạng thái',
+        sortable: true,
+        width: 60,
+        canAutoResize: true,
+        headerClass: 'd-flex justify-content-center',
+        cellClass: 'd-flex justify-content-center',
+      },
+      {
+        prop: 'action',
+        width: 60,
+        name: 'Hành động',
+        sortable: false,
+        canAutoResize: true,
+        cellTemplate: this.actionTemplate,
+        headerClass: 'd-flex justify-content-center',
+        cellClass: 'd-flex justify-content-center',
+      },
+    ];
+  }
+
+  columns: TableColumn[] = [];
+  @ViewChild(DatatableComponent) table!: DatatableComponent;
+  status: { id: string; value: string }[] = [];
+
+  get metadata$(): Observable<PagingMetadata> {
+    return this.commentListState.select('metadata');
+  }
+  get loading$(): Observable<boolean> {
+    return this.commentListState.select('loading');
+  }
+  get comments$(): Observable<Comment[]> {
+    return this.commentListState.select('comments');
+  }
+
+  get metadataComment$(): Observable<PagingMetadata> {
+    return this.commentListState.select('metadata');
+  }
+  get loadingComment$(): Observable<boolean> {
+    return this.commentListState.select('loading');
+  }
+
+  searchComment$ = new BehaviorSubject<SearchInfo>({});
+  searchFormComment = new FormGroup({
+    keyword: new FormControl(),
+    isFeedbackApproved: new FormControl(),
+  });
+
+  submitSearchComment$ = new Subject<
+    Partial<{ keyword: string; isFeedbackApproved: string }>
+  >();
+  resetSearchComment$ = new Subject<void>();
+
+  onPage(paging: PageInfo) {
+    this.searchComment$.next({
+      ...this.searchComment$.getValue(),
+      currentPage: paging.offset,
+    });
+  }
+  onSort(event: SortInfo) {
+    this.table.offset - 1;
+    this.searchComment$.next({
+      ...this.searchComment$.getValue(),
+      sort: { sortBy: event.column.prop, dir: event.newValue },
+    });
   }
 
 }
