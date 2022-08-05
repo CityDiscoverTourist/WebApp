90b47820e94e9d418a789bcd388518f5b6825b40
ssangnd <75923274+ssangnd@users.noreply.github.com>
2022-08-05 19:57:14 +0700

    ui proccess logic chat

---
+++ src/app/pages/chat-box/chat-box.component.ts
@@ -0,0 +1,75 @@
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  EventEmitter,
+  HostListener,
+  Input, OnInit,
+  Output,
+  ViewChild
+} from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import {  Message } from 'src/app/models';
+import { NgForm } from '@angular/forms';
+import { MessageService } from 'src/app/services';
+
+@Component({
+  selector: 'app-chat-box',
+  templateUrl: './chat-box.component.html',
+  styleUrls: ['./chat-box.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ChatBoxComponent implements OnInit, AfterViewInit {
+  @Input() user: Message;//information of chat box
+  messageContent: string;
+  @ViewChild('ChatBox', { static: true }) element: ElementRef;
+  @Input() right: number;
+  @Output() removeChatBox = new EventEmitter();
+  @Output() activedChatBoxEvent = new EventEmitter();
+  isCollapsed = false;
+  @ViewChild('messageForm') messageForm: NgForm;
+  // @ViewChild(ScrollToBottomDirective) scroll: ScrollToBottomDirective;
+
+  constructor(public messageService: MessageService) {
+  
+  }
+
+  ngOnInit(): void {
+    // this.messageService.connect();
+  }
+  ngAfterViewInit() {
+    var chatBox = document.getElementById(this.user.User)!;
+    chatBox.style.right = this.right + "px";
+  }
+  @HostListener("scroll", ["$event"])
+  onScroll(event:any) {
+    let pos = event.target.scrollTop + event.target.offsetHeight;
+    let max = event.target.scrollHeight;
+    //pos/max will give you the distance between scroll bottom and and bottom of screen in percentage.
+    // if (pos == max) {
+    //   this.messageService.seenMessage(this.user.userName);
+    // }
+  }
+
+  sendMessage(conId:string): void {  
+    this.messageService.sendMessageToHub(conId,this.messageContent,`${'admin'+this.user.User}`);
+  }
+  closeBoxChat() {
+    this.removeChatBox.emit(this.user.User);
+  }
+
+  onFocusEvent(event: any) {
+    //console.log(event.target.value);
+    this.activedChatBox();
+  }
+
+  activedChatBox() {
+    this.activedChatBoxEvent.emit(this.user.User)
+  }
+
+  isTrue(msgValue:string, msgUser:string){
+    return msgValue.includes(msgUser);
+  }
+
+}
