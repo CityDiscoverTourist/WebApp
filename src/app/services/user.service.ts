d1c456f25d9c87239f5d6064c41677d0a4a28baa
ssangnd <75923274+ssangnd@users.noreply.github.com>
2022-08-17 23:06:56 +0700

    add register service

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,25 +1,41 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
 import { UserToken } from '../models';
+import { BaseService } from './base.service';
 
 @Injectable({
   providedIn: 'root',
 })
-export class UserService {
-  constructor(private httpClient: HttpClient) {}
+export class UserService extends BaseService {
+  private _sharedHeaders = new HttpHeaders();
+  constructor(private httpClient: HttpClient) {
+    super();
+  }
+  
+
   login(email: string, password: string): Observable<UserToken> {
     return this.httpClient.post<any>(
       `${environment.apiUrl}/api/v1/auths/login-admin`,
-      { email, password }
+      {email,password},
+      { headers: this._sharedHeaders }
     );
   }
 
-  forgetPassword(email: string) {   
+  forgetPassword(email: string) {
     return this.httpClient.post<any>(
       `${environment.apiUrl}/api/v1/auths/forgot-password?email=${email}`,
-      ''
+      { headers: this._sharedHeaders }
+    );
+  }
+
+  addAdminAccount(email: string, password: string):Observable<boolean>{
+
+    return this.httpClient.post<boolean>(
+      `${environment.apiUrl}/api/v1/auths/register-admin`,
+      {email,password},
+      { headers: this._sharedHeaders }
     );
   }
 }
