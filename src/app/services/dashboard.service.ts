ba8424bcffef7fb17da5b02143a56ca6ce7a2b13
ssangnd <75923274+ssangnd@users.noreply.github.com>
2022-08-20 22:36:19 +0700

    add top quest month/year service

--- src/app/services/dashboard.service.ts
+++ src/app/services/dashboard.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { stringify } from 'query-string';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
 import { BaseService } from './base.service';
@@ -50,9 +51,28 @@ export class DashboardService extends BaseService {
     );
   }
 
-  getTotalRevenueByYear(year:string):Observable<number[]> {
+  getTotalRevenueByYear(year: string): Observable<number[]> {
     return this.http.get<number[]>(
       `${environment.apiUrl}/api/v1/dashboards/revenue-all-month?year=${year}`,
+      { headers: this._sharedHeaders }
+    );
+  }
+  getTopQuestByMonth(month: string, year: string): Observable<any[]> {
+    const query = stringify({
+      month: month != null ? month : undefined,
+      year: year != null ? year : undefined,
+    });
+    return this.http.get<number[]>(
+      `${environment.apiUrl}/api/v1/dashboards/top-quest-by-month?` + query,
+      { headers: this._sharedHeaders }
+    );
+  }
+  getTopQuestByYear(year: string): Observable<any> {
+    const query = stringify({
+      year: year != null ? year : undefined,
+    });
+    return this.http.get<number[]>(
+      `${environment.apiUrl}/api/v1/dashboards/top-quest-by-year?` + query,
       { headers: this._sharedHeaders }
     );
   }
