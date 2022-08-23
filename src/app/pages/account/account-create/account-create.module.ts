4d04a70c56cf81e10328fd8c9e737656c80d8a89
ssangnd <75923274+ssangnd@users.noreply.github.com>
2022-08-23 17:19:35 +0700

    add module ngselect

--- src/app/pages/account/account-create/account-create.module.ts
+++ src/app/pages/account/account-create/account-create.module.ts
@@ -4,11 +4,15 @@ import { CommonModule } from '@angular/common';
 import { AccountCreateRoutingModule } from './account-create-routing.module';
 import { AccountCreateComponent } from './account-create.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgSelectModule } from '@ng-select/ng-select';
 
 
 
+const lib = [
+  NgSelectModule,
+];
 @NgModule({
   declarations: [AccountCreateComponent],
-  imports: [CommonModule, AccountCreateRoutingModule,FormsModule, ReactiveFormsModule],
+  imports: [CommonModule, AccountCreateRoutingModule,FormsModule, ReactiveFormsModule,...lib],
 })
 export class AccountCreateModule {}
