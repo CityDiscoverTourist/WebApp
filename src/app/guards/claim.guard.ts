4765fe5f75f821504a01bd82edd663424a2c965d
ssangnd <75923274+ssangnd@users.noreply.github.com>
2022-08-23 18:38:01 +0700

    add claim guard role

---
+++ src/app/guards/claim.guard.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ClaimGuard implements CanActivate {
+  constructor(private router: Router) {}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    if (localStorage.getItem('role') == 'QuestOwner') {
+      if (state.url.includes('account') || state.url.includes('customer')) {
+        this.router.navigate(['admin/dashboard']);
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
